98f7e2b254faa7064fce5d103d7439394439697e
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-08-03 23:28:05 +0000

    Add ability to generate random string for shortURL

--- express_server.js
+++ express_server.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+const generateRandomString() {
+  let randomString = Math.random().toString(36).slice(2, 8);
+  return randomString;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
